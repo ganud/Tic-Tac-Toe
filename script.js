c30e0e22e6abdb36ea646c160ac49521dd50a7d6
Dundo <107737568+ganud@users.noreply.github.com>
2023-09-14 21:26:15 -0400

    Javascript module and factory setup plus more boilerplate

---
+++ script.js
@@ -0,0 +1,21 @@
+var gameBoard = (function() {
+    let board = [
+        [0, 0, 0,],
+        [0, 0, 0,],
+        [0, 0, 0,], 
+    ];
+    const writeBoard = () => {
+        
+    }
+})();
+
+var displayController = (function() {
+
+})();
+
+function newPlayer(symbol, name) {
+    return {symbol, name};
+};
+
+let Player1 = newPlayer("O", "Player 1")
+let Player2 = newPlayer("O", "Player 2")
