d5c565ccafef84c2a71b896fa85a502a31664900
Dundo <107737568+ganud@users.noreply.github.com>
2023-09-14 21:45:00 -0400

    Writeboard function complete

--- script.js
+++ script.js
@@ -5,8 +5,19 @@ var gameBoard = (function() {
         [0, 0, 0,], 
     ];
     const writeBoard = () => {
-        
+        const boardgrid = document.getElementsByClassName("board")[0]
+        // Iterate through grid
+        for (row of board) {
+            for (item of row) {
+                let new_item = document.createElement("div");
+                new_item.className = "board-item";
+                new_item.innerHTML = item;
+                boardgrid.appendChild(new_item);
+            }
+        }
     }
+
+    return {writeBoard}
 })();
 
 var displayController = (function() {
